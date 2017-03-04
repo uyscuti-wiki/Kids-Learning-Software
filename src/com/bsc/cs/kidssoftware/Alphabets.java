139eea84ff0799cf45803ff6d3ee67683c74e2cb
uyscuti <uyscuti.wiki@gmail.com>
2017-03-04 18:04:19 +0530

    Refactored code

---
+++ src/com/bsc/cs/kidssoftware/Alphabets.java
@@ -0,0 +1,189 @@
+package com.bsc.cs.kidssoftware;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+public class Alphabets {
+
+	private JButton[] alphabets = new JButton[26];
+	private JLabel label = new JLabel("Kid's Learning Software");
+	private String[] names = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
+			"S", "T", "U", "V", "W", "X", "Y", "Z" };
+
+	public Alphabets(JPanel alphabets_jp) {
+
+		label.setHorizontalAlignment(SwingConstants.CENTER);
+		label.setForeground(Color.BLUE);
+		label.setFont(new Font("Times New Roman", Font.PLAIN, 34));
+
+		for (int i = 0; i < alphabets.length; i++) {
+			alphabets[i] = new JButton(names[i]);
+			alphabets[i].setBorder(BorderFactory.createLineBorder(Color.black));
+		}
+
+		GroupLayout gl_panel1 = new GroupLayout(alphabets_jp);
+
+		gl_panel1.setHorizontalGroup(gl_panel1.createParallelGroup(Alignment.LEADING).addGroup(gl_panel1
+				.createSequentialGroup().addContainerGap()
+				.addGroup(gl_panel1.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel1.createSequentialGroup()
+								.addComponent(alphabets[0], GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+								.addGap(42).addComponent(alphabets[13], GroupLayout.PREFERRED_SIZE, 25,
+										GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panel1.createSequentialGroup()
+								.addComponent(alphabets[1], GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+								.addGap(42).addComponent(alphabets[14], GroupLayout.PREFERRED_SIZE, 25,
+										GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panel1.createSequentialGroup()
+								.addComponent(alphabets[2], GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+								.addGap(42).addComponent(alphabets[15], GroupLayout.PREFERRED_SIZE, 25,
+										GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panel1.createSequentialGroup()
+								.addComponent(alphabets[3], GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+								.addGap(42).addComponent(alphabets[16], GroupLayout.PREFERRED_SIZE, 25,
+										GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panel1.createSequentialGroup()
+								.addComponent(alphabets[4], GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+								.addGap(42).addComponent(alphabets[17], GroupLayout.PREFERRED_SIZE, 25,
+										GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panel1.createSequentialGroup()
+								.addComponent(alphabets[5], GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+								.addGap(42).addComponent(alphabets[18], GroupLayout.PREFERRED_SIZE, 25,
+										GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panel1.createSequentialGroup()
+								.addComponent(alphabets[6], GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+								.addGap(42).addComponent(alphabets[19], GroupLayout.PREFERRED_SIZE, 25,
+										GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panel1.createSequentialGroup()
+								.addComponent(alphabets[7], GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+								.addGap(42).addComponent(alphabets[20], GroupLayout.PREFERRED_SIZE, 25,
+										GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panel1.createSequentialGroup()
+								.addComponent(alphabets[8], GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+								.addGap(42).addComponent(alphabets[21], GroupLayout.PREFERRED_SIZE, 25,
+										GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panel1.createSequentialGroup()
+								.addComponent(alphabets[9], GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+								.addGap(42).addComponent(alphabets[22], GroupLayout.PREFERRED_SIZE, 25,
+										GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panel1.createSequentialGroup()
+								.addComponent(alphabets[10], GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+								.addGap(42).addComponent(alphabets[23], GroupLayout.PREFERRED_SIZE, 25,
+										GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panel1.createSequentialGroup()
+								.addComponent(alphabets[11], GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+								.addGap(42).addComponent(alphabets[24], GroupLayout.PREFERRED_SIZE, 25,
+										GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panel1.createSequentialGroup()
+								.addComponent(alphabets[12], GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+								.addGap(42).addComponent(alphabets[25], GroupLayout.PREFERRED_SIZE, 25,
+										GroupLayout.PREFERRED_SIZE)))
+				.addContainerGap(50, Short.MAX_VALUE))
+				.addGroup(gl_panel1.createSequentialGroup().addContainerGap(201, Short.MAX_VALUE).addComponent(label)
+						.addGap(193)));
+		gl_panel1.setVerticalGroup(gl_panel1.createParallelGroup(Alignment.LEADING).addGroup(gl_panel1
+				.createSequentialGroup().addGap(20).addComponent(label).addContainerGap()
+				.addGroup(gl_panel1.createParallelGroup(Alignment.BASELINE)
+						.addComponent(alphabets[0], GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+						.addComponent(alphabets[13], GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE))
+				.addGap(5)
+				.addGroup(gl_panel1.createParallelGroup(Alignment.BASELINE)
+						.addComponent(alphabets[1], GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+						.addComponent(alphabets[14], GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE))
+				.addGap(5)
+				.addGroup(gl_panel1.createParallelGroup(Alignment.BASELINE)
+						.addComponent(alphabets[2], GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+						.addComponent(alphabets[15], GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE))
+				.addGap(5)
+				.addGroup(gl_panel1.createParallelGroup(Alignment.BASELINE)
+						.addComponent(alphabets[3], GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+						.addComponent(alphabets[16], GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE))
+				.addGap(5)
+				.addGroup(gl_panel1.createParallelGroup(Alignment.BASELINE)
+						.addComponent(alphabets[4], GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+						.addComponent(alphabets[17], GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE))
+				.addGap(5)
+				.addGroup(gl_panel1.createParallelGroup(Alignment.BASELINE)
+						.addComponent(alphabets[5], GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+						.addComponent(alphabets[18], GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE))
+				.addGap(5)
+				.addGroup(gl_panel1.createParallelGroup(Alignment.BASELINE)
+						.addComponent(alphabets[6], GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+						.addComponent(alphabets[19], GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE))
+				.addGap(5)
+				.addGroup(gl_panel1.createParallelGroup(Alignment.BASELINE)
+						.addComponent(alphabets[7], GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+						.addComponent(alphabets[20], GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE))
+				.addGap(5)
+				.addGroup(gl_panel1.createParallelGroup(Alignment.BASELINE)
+						.addComponent(alphabets[8], GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+						.addComponent(alphabets[21], GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE))
+				.addGap(5)
+				.addGroup(gl_panel1.createParallelGroup(Alignment.BASELINE)
+						.addComponent(alphabets[9], GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+						.addComponent(alphabets[22], GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE))
+				.addGap(5)
+				.addGroup(gl_panel1.createParallelGroup(Alignment.BASELINE)
+						.addComponent(alphabets[10], GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+						.addComponent(alphabets[23], GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE))
+				.addGap(5)
+				.addGroup(gl_panel1.createParallelGroup(Alignment.BASELINE)
+						.addComponent(alphabets[11], GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+						.addComponent(alphabets[24], GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE))
+				.addGap(5)
+				.addGroup(gl_panel1.createParallelGroup(Alignment.BASELINE)
+						.addComponent(alphabets[12], GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+						.addComponent(alphabets[25], GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE))
+				.addGap(5).addContainerGap(43, Short.MAX_VALUE)));
+		alphabets_jp.setLayout(gl_panel1);
+		
+		final JLabel lblNewLabel_1 = new JLabel();//thanks! np.. make sure to fill assets with  C D etc.. yeah I will! wait tha ts it? Is there something I can do to auto commit to git? from eclipse? yes
+
+		for (int i = 0; i < alphabets.length; i++) {
+			final int n = i;
+
+			alphabets[i].addActionListener(new ActionListener() {
+
+				public void actionPerformed(ActionEvent ae) {
+					try {
+
+						BufferedImage img = ImageIO.read(new File("assets/" + names[n] + ".jpg"));
+						Image dimg = img.getScaledInstance(325, 350, Image.SCALE_SMOOTH);
+						ImageIcon imageIcon = new ImageIcon(dimg);
+						lblNewLabel_1.setIcon(imageIcon);
+						gl_panel1.setHorizontalGroup(gl_panel1.createParallelGroup(Alignment.LEADING)
+								.addGroup(gl_panel1.createSequentialGroup()
+										.addContainerGap(247, Short.MAX_VALUE).addComponent(lblNewLabel_1,
+												GroupLayout.PREFERRED_SIZE, 325, GroupLayout.PREFERRED_SIZE)
+										.addContainerGap()));
+						gl_panel1.setVerticalGroup(gl_panel1.createParallelGroup(Alignment.LEADING)
+								.addGroup(gl_panel1
+										.createSequentialGroup().addGap(65).addComponent(lblNewLabel_1,
+												GroupLayout.PREFERRED_SIZE, 350, GroupLayout.PREFERRED_SIZE)
+										.addContainerGap(100, Short.MAX_VALUE)));
+						alphabets_jp.setLayout(gl_panel1);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			});
+		}
+
+	}
+
+}
--- src/com/bsc/cs/kidssoftware/WelcomeModule.java
+++ src/com/bsc/cs/kidssoftware/WelcomeModule.java
@@ -1,170 +1,148 @@
 package com.bsc.cs.kidssoftware;
 
+import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.EventQueue;
 import java.awt.Font;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JMenu;
 import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.SwingConstants;
 
-import org.eclipse.wb.swing.FocusTraversalOnArray;
+public class WelcomeModule extends JFrame {
 
-public class WelcomeModule {
+    private static final long serialVersionUID = 1L;
+    private JPanel cardPanel, welcome_jp, alphabets_jp, numbers_jp, colors_jp, wanimals_jp, danimals_jp, birds_jp, fruits_jp, vegetables_jp, flowers_jp;
+    private JButton alphabets_btn, colors_btn, numbers_btn, wanimals_btn, danimals_btn, birds_btn, fruits_btn, vegetables_btn, flowers_btn;
+    private CardLayout cardLayout = new CardLayout();
+    private JMenuBar menuBar;
+    
 
-	private JFrame frame;
+    public WelcomeModule() {
+        cardPanel = new JPanel();
+        cardPanel.setLayout(cardLayout);
+        
+        JLabel label = new JLabel("Kid's Learning Software");
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+        label.setForeground(Color.BLUE);
+        label.setFont(new Font("Times New Roman", Font.PLAIN, 34));
+        
+        setTitle("Kid's Learning Software");
+        setSize(752, 496);
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					WelcomeModule window = new WelcomeModule();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+        welcome_jp = new JPanel();
+        alphabets_jp = new JPanel();
+        numbers_jp = new JPanel();
+        colors_jp = new JPanel();
+        wanimals_jp = new JPanel();
+        danimals_jp = new JPanel();
+        birds_jp = new JPanel();
+        fruits_jp = new JPanel();
+        vegetables_jp = new JPanel();
+        flowers_jp = new JPanel();
+        
+        cardPanel.add(welcome_jp, "1");
+        
+        alphabets_btn = new JButton("Alphabets");
+        colors_btn = new JButton("Colors");
+        numbers_btn = new JButton("Numbers");
+        wanimals_btn = new JButton("Wild Animals");
+        
+        danimals_btn = new JButton("Domestic Animals");
+        danimals_btn.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+        
+        birds_btn = new JButton("Birds");
+        fruits_btn = new JButton("Fruits");
+        vegetables_btn = new JButton("Vegetables");
+        flowers_btn = new JButton("Flowers");
+        
+        GroupLayout gl_panel = new GroupLayout(welcome_jp);
+        gl_panel.setHorizontalGroup(
+        	gl_panel.createParallelGroup(Alignment.TRAILING)
+        		.addGroup(gl_panel.createSequentialGroup()
+        			.addGap(62)
+        			.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+        				.addComponent(alphabets_btn, GroupLayout.PREFERRED_SIZE, 110, GroupLayout.PREFERRED_SIZE)
+        				.addComponent(wanimals_btn, GroupLayout.PREFERRED_SIZE, 110, GroupLayout.PREFERRED_SIZE)
+        				.addComponent(fruits_btn, GroupLayout.PREFERRED_SIZE, 110, GroupLayout.PREFERRED_SIZE))
+        			.addGap(130)
+        			.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+        				.addComponent(danimals_btn, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        				.addGroup(gl_panel.createParallelGroup(Alignment.LEADING, false)
+        					.addComponent(colors_btn, GroupLayout.DEFAULT_SIZE, 110, Short.MAX_VALUE)
+        					.addComponent(vegetables_btn, GroupLayout.DEFAULT_SIZE, 110, Short.MAX_VALUE)))
+        			.addPreferredGap(ComponentPlacement.RELATED, 149, Short.MAX_VALUE)
+        			.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING)
+        				.addComponent(numbers_btn, GroupLayout.PREFERRED_SIZE, 110, GroupLayout.PREFERRED_SIZE)
+        				.addComponent(birds_btn, GroupLayout.PREFERRED_SIZE, 110, GroupLayout.PREFERRED_SIZE)
+        				.addComponent(flowers_btn, GroupLayout.PREFERRED_SIZE, 110, GroupLayout.PREFERRED_SIZE))
+        			.addContainerGap(65, Short.MAX_VALUE))
+        		.addGroup(gl_panel.createSequentialGroup()
+        			.addContainerGap(214, Short.MAX_VALUE)
+        			.addComponent(label)
+        			.addGap(193))
+        );
+        gl_panel.setVerticalGroup(
+        	gl_panel.createParallelGroup(Alignment.LEADING)
+        		.addGroup(gl_panel.createSequentialGroup()
+        			.addGap(30)
+        			.addComponent(label)
+        			.addGap(53)
+        			.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+        				.addComponent(alphabets_btn, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE)
+        				.addComponent(colors_btn, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE)
+        				.addComponent(numbers_btn, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE))
+        			.addGap(53)
+        			.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+        				.addComponent(wanimals_btn, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE)
+        				.addComponent(danimals_btn, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE)
+        				.addComponent(birds_btn, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE))
+        			.addGap(53)
+        			.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+        				.addComponent(fruits_btn, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE)
+        				.addComponent(vegetables_btn, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE)
+        				.addComponent(flowers_btn, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE))
+        			.addContainerGap(72, Short.MAX_VALUE))
+        );
+        
+        welcome_jp.setLayout(gl_panel);
+        
+        alphabets_btn.addActionListener(new ActionListener() {
 
-	/**
-	 * Create the application.
-	 */
-	public WelcomeModule() {
-		initialize();
-	}
+            public void actionPerformed(ActionEvent e) {
+                cardLayout.show(cardPanel, "2");
+            }
+        });
+        
+        new Alphabets(alphabets_jp);
+        cardPanel.add(alphabets_jp, "2");
+             
+        getContentPane().add(cardPanel, BorderLayout.CENTER);
+        
+        menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+    }
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setResizable(false);
-		frame.getContentPane().setBackground(new Color(147, 112, 219));
-		frame.setBounds(100, 100, 729, 445);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new CardLayout(0, 0));
-		
-		JPanel panel = new JPanel();
-		
-		JButton button = new JButton("New button");
-		
-		JButton button_1 = new JButton("New button");
-		
-		JButton btnAlphabets = new JButton("Alphabets");
-		
-		JButton button_3 = new JButton("New button");
-		
-		JButton button_4 = new JButton("New button");
-		
-		JButton button_5 = new JButton("New button");
-		
-		JLabel label = new JLabel("Kid's Learning Software");
-		label.setHorizontalAlignment(SwingConstants.CENTER);
-		label.setForeground(Color.BLACK);
-		label.setFont(new Font("Times New Roman", Font.PLAIN, 34));
-		
-		JButton button_6 = new JButton("New button");
-		
-		JButton button_7 = new JButton("New button");
-		
-		JButton btnNewButton = new JButton("New button");
-		GroupLayout gl_panel = new GroupLayout(panel);
-		gl_panel.setHorizontalGroup(
-			gl_panel.createParallelGroup(Alignment.TRAILING)
-				.addGroup(gl_panel.createSequentialGroup()
-					.addGap(62)
-					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
-						.addComponent(button_1, GroupLayout.PREFERRED_SIZE, 110, GroupLayout.PREFERRED_SIZE)
-						.addComponent(button, GroupLayout.PREFERRED_SIZE, 110, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnAlphabets, GroupLayout.PREFERRED_SIZE, 110, GroupLayout.PREFERRED_SIZE))
-					.addGap(132)
-					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
-						.addComponent(button_4, GroupLayout.PREFERRED_SIZE, 110, GroupLayout.PREFERRED_SIZE)
-						.addComponent(button_7, GroupLayout.PREFERRED_SIZE, 110, GroupLayout.PREFERRED_SIZE)
-						.addComponent(button_5, GroupLayout.PREFERRED_SIZE, 110, GroupLayout.PREFERRED_SIZE))
-					.addPreferredGap(ComponentPlacement.RELATED, 144, Short.MAX_VALUE)
-					.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING)
-						.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 110, GroupLayout.PREFERRED_SIZE)
-						.addComponent(button_6, GroupLayout.PREFERRED_SIZE, 110, GroupLayout.PREFERRED_SIZE)
-						.addComponent(button_3, GroupLayout.PREFERRED_SIZE, 110, GroupLayout.PREFERRED_SIZE))
-					.addContainerGap(54, Short.MAX_VALUE))
-				.addGroup(gl_panel.createSequentialGroup()
-					.addContainerGap(201, Short.MAX_VALUE)
-					.addComponent(label)
-					.addGap(193))
-		);
-		gl_panel.setVerticalGroup(
-			gl_panel.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel.createSequentialGroup()
-					.addGap(30)
-					.addComponent(label)
-					.addGap(18)
-					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
-						.addComponent(button_4, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE)
-						.addComponent(button_3, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnAlphabets, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE))
-					.addGap(42)
-					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
-						.addComponent(button, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE)
-						.addComponent(button_7, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE)
-						.addComponent(button_6, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE))
-					.addGap(43)
-					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
-						.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE)
-						.addComponent(button_5, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE)
-						.addComponent(button_1, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE))
-					.addContainerGap(66, Short.MAX_VALUE))
-		);
-		panel.setLayout(gl_panel);
-		frame.getContentPane().add(panel, "l1");
-		frame.getContentPane().setFocusTraversalPolicy(new FocusTraversalOnArray(new Component[]{panel}));
-		
-		JPanel panel_1 = new JPanel();
-		frame.getContentPane().add(panel_1, "l2");
-		GroupLayout gl_panel_1 = new GroupLayout(panel_1);
-		gl_panel_1.setHorizontalGroup(
-			gl_panel_1.createParallelGroup(Alignment.LEADING)
-				.addGap(0, 723, Short.MAX_VALUE)
-		);
-		gl_panel_1.setVerticalGroup(
-			gl_panel_1.createParallelGroup(Alignment.LEADING)
-				.addGap(0, 395, Short.MAX_VALUE)
-		);
-		panel_1.setLayout(gl_panel_1);
-		
-		JMenuBar menuBar = new JMenuBar();
-		frame.setJMenuBar(menuBar);
-		
-		JMenu mnFile = new JMenu("   File");
-		mnFile.setHorizontalAlignment(SwingConstants.CENTER);
-		menuBar.add(mnFile);
-		
-		JMenuItem mntmHome = new JMenuItem("Home");
-		mnFile.add(mntmHome);
-		
-		JMenuItem mntmNewMenuItem = new JMenuItem("Exit             ");
-		mntmNewMenuItem.addMouseListener(new MouseAdapter() {
-			@Override
-			System.exit(0);
-			public void mouseClicked(MouseEvent e) {
-			}
-		});
-		mnFile.add(mntmNewMenuItem);
-	}
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+            	WelcomeModule frame = new WelcomeModule();
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+                frame.setResizable(false);
+            }
+        });
+    }
 }
